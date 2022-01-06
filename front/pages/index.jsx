e0d64478628af6ba088794d2bdfdf0ae2290fb63
Etienne Fontaine <etienne.fontaine@blablacar.com>
2022-01-06 10:06:20 +0100

    [Actions] Use Github Packages

--- front/pages/index.jsx
+++ front/pages/index.jsx
@@ -1,21 +1,11 @@
 import getConfig from "next/config";
 import Layout from '../components/Layout'
 import Alert from '../components/Alert'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "../components/Redirections/Modal";
 import Form from "../components/Redirections/Form";
 import Table from "../components/Redirections/Table";
 const { publicRuntimeConfig } = getConfig();
-
-export async function getStaticProps() {
-  const payload = await fetch(`${publicRuntimeConfig.API_URL}api/config`).then(async (res) => await res.json());
-
-  return {
-    props: {
-      payload
-    },
-  }
-}
 
 export const defaultFormValues = {
   pushGithub: false,
@@ -32,12 +22,18 @@ export const defaultPayload = {
   content: ''
 }
 
-export default function Home({ payload }) {
+export default function Home() {
 
   const [CSVData, setCSVData] = useState([])
+  const [apiResponse, setAPIResponse] = useState()
   const [alert, setAlert] = useState(defaultPayload)
   const [modal, setModal] = useState(defaultPayload)
   const [formData, setFormData] = useState(defaultFormValues);
+
+  useEffect(async () => {
+    const payload = await fetch(`${publicRuntimeConfig.API_URL}api/config`).then(async (res) => await res.json());
+    setAPIResponse(payload)
+  }, [])
 
   return (
     <Layout>
@@ -49,7 +45,7 @@ export default function Home({ payload }) {
             <div className="flex justify-between">
               <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">istio-redirector</h2>
             </div>
-            <Form sourceInfo={payload} setAlert={setAlert} setModal={setModal} setCSVData={setCSVData} CSVData={CSVData} setFormData={setFormData} formData={formData} />
+            {apiResponse && <Form sourceInfo={apiResponse} setAlert={setAlert} setModal={setModal} setCSVData={setCSVData} CSVData={CSVData} setFormData={setFormData} formData={formData} />}
             <Table CSVData={CSVData} redirectionType={formData.redirection_type} />
           </div>
         </div>
